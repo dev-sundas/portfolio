fe23a743d3eecc7e444fb3c469a45139edae1347
dev-sundas <hassan922837@gmail.com>
2024-08-30 11:25:33 +0500

    changes in header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,13 @@ export default function Header ({headerdata}:Props) {
     {headerdata.map((item)=>(
        <div key={item.id} className='flex flex-col gap-5  justify-between py-10 w-[40%] md:flex-row md:items-center md:w-full '>
        <h1 className='text-[27px] md:text-[32px] font-bold inline leading-3  tracking-wide'>{item.heading}</h1>
-       <Link href={item.link} className=' border-[2px] border-white rounded-sm p-2 hover:text-black hover:bg-white font-semibold ease-in-out transition-all'>{item.linkText}</Link>
+       <nav>
+        <ul>
+          <li>
+          <Link href={item.link} className=' border-[2px] border-white rounded-sm p-2 hover:text-black hover:bg-white font-semibold ease-in-out transition-all'>{item.linkText}</Link>
+          </li>
+        </ul>
+       </nav>
      </div>
     ))}
    </header>
